5a7cf9e5220a11147449181054a97c2990c0c35d
Colin-Posat <colinposat@gmail.com>
2025-11-03 08:59:15 -0800

    shim added

---
+++ types/next-types-shim.d.ts
@@ -0,0 +1,8 @@
+declare module 'next/types.js' {
+    export type ResolvingMetadata = import('next').ResolvingMetadata;
+    export * from 'next';
+}
+declare module 'next/types' {
+    export type ResolvingMetadata = import('next').ResolvingMetadata;
+    export * from 'next';
+}
