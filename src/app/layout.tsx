24cd4c1e3151b3443755d17aa5240c92f5f894e8
Colin-Posat <colinposat@gmail.com>
2025-12-19 00:32:23 -0800

    staygenie maybe shows up now

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,27 +58,47 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Analytics (Global site tag) */}
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-3C4PDTEN4G"
-        />
-        <Script id="ga-gtag-init" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-3C4PDTEN4G');
-          `}
-        </Script>
+  {/* Google Analytics */}
+  <Script
+    async
+    src="https://www.googletagmanager.com/gtag/js?id=G-3C4PDTEN4G"
+  />
+  <Script id="ga-gtag-init" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-3C4PDTEN4G');
+    `}
+  </Script>
 
-        {/* Extra meta tags for browser + Google favicon detection */}
-        <meta name="agd-partner-manual-verification" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#1df9ff" />
-      </head>
+  {/* Extra meta tags */}
+  <meta name="agd-partner-manual-verification" />
+  <link rel="icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+  <link rel="manifest" href="/manifest.json" />
+  <meta name="theme-color" content="#1df9ff" />
+
+  {/* Organization Schema */}
+  <script
+    type="application/ld+json"
+    dangerouslySetInnerHTML={{
+      __html: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "StayGenie",
+        "alternateName": "Stay Genie",
+        "url": "https://www.staygenie.app",
+        "logo": "https://www.staygenie.app/images/staygenielogo.png",
+        "sameAs": [
+          "https://instagram.com/staygenieapp",
+          "https://www.staygenie.app"
+        ]
+      }),
+    }}
+  />
+</head>
+
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
